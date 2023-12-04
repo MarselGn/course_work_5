e351b4306f02a5c212b8fe096ebc2eea4451f943
MarselGn <marsel69.ru@mail.ru>
2023-12-05 03:08:40 +0500

    add func

---
+++ DBCreator.py
@@ -0,0 +1,85 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from psycopg2 import Error
+import os
+
+
+class DBCreator:
+    """
+    Класс для создания БД
+    """
+
+    def __init__(self):
+        self.user = "postgres"
+        self.password = os.getenv('PSWRD')
+        self.host = "localhost"
+
+    def create_db(self, db_name):
+        """
+        Создание базы данных
+        """
+        try:
+            conn = psycopg2.connect(user=self.user,
+                                    password=self.password,
+                                    host=self.host)
+            conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+            cursor = conn.cursor()
+            sql_create_database = f"create database {db_name}"
+            cursor.execute(sql_create_database)
+        except (Exception, Error) as error:
+            print("Ошибка при работе с PostgreSQL", error)
+        finally:
+            if conn:
+                cursor.close()
+                conn.close()
+
+    def create_table(self, db_name, table_name):
+        """
+        Создание таблицы
+        """
+        try:
+            conn = psycopg2.connect(user=self.user,
+                                    password=self.password,
+                                    host=self.host,
+                                    database=f"{db_name}")
+            cursor = conn.cursor()
+            create_table_query = """ CREATE TABLE """f'{table_name}'"""
+                                     (
+                                         title text,
+                                         url text,
+                                         salary_from int,
+                                         employer text,
+                                         employer_id int
+                                     )"""
+            cursor.execute(create_table_query)
+            conn.commit()
+        except (Exception, Error) as error:
+            print("Ошибка при работе с PostgreSQL", error)
+        finally:
+            if conn:
+                cursor.close()
+                conn.close()
+        pass
+
+    def fill_table(self, db_name, vacancies_list):
+        """
+        Заполнение таблицы
+        """
+        try:
+            conn = psycopg2.connect(user=self.user,
+                                    password=self.password,
+                                    host=self.host,
+                                    database=f"{db_name}")
+
+            cursor = conn.cursor()
+            for vacancy in vacancies_list:
+                cursor.execute("INSERT INTO vacancies VALUES (%s, %s, %s, %s, %s)",
+                               (vacancy["title"], vacancy["url"], vacancy["salary_from"],
+                                vacancy["employer"], vacancy["employer_id"]))
+            conn.commit()
+        except (Exception, Error) as error:
+            print("Ошибка при работе с PostgreSQL", error)
+        finally:
+            if conn:
+                cursor.close()
+                conn.close()
---
+++ DBManager.py
@@ -0,0 +1,127 @@
+import psycopg2
+from psycopg2 import Error
+import os
+
+
+class DBManager:
+    """
+    Класс для работы с БД
+    """
+
+    def __init__(self, db_name):
+        self.user = "postgres"
+        self.password = os.getenv('PSWRD')
+        self.host = "localhost"
+        self.database = db_name
+
+    def get_companies_and_vacancies_count(self):
+        """
+        получает список всех компаний и количество вакансий у каждой компании
+        """
+        try:
+            conn = psycopg2.connect(user=self.user,
+                                    password=self.password,
+                                    host=self.host,
+                                    database=self.database)
+
+            cursor = conn.cursor()
+            query = """SELECT employer, COUNT(*)
+                       FROM vacancies
+                       GROUP BY employer"""
+            cursor.execute(query)
+            print(cursor.fetchall())
+        except (Exception, Error) as error:
+            print("Ошибка при работе с PostgreSQL", error)
+        finally:
+            if conn:
+                cursor.close()
+                conn.close()
+
+    def get_all_vacancies(self):
+        """
+        получает список всех вакансий с указанием названия компании,
+        названия вакансии и зарплаты и ссылки на вакансию
+        """
+        try:
+            conn = psycopg2.connect(user=self.user,
+                                    password=self.password,
+                                    host=self.host,
+                                    database=self.database)
+
+            cursor = conn.cursor()
+            query = """SELECT employer, title, salary_from, url
+                              FROM vacancies"""
+            cursor.execute(query)
+            print(cursor.fetchall())
+        except (Exception, Error) as error:
+            print("Ошибка при работе с PostgreSQL", error)
+        finally:
+            if conn:
+                cursor.close()
+                conn.close()
+
+    def get_avg_salary(self):
+        """
+        получает среднюю зарплату по вакансиям
+        """
+        try:
+            conn = psycopg2.connect(user=self.user,
+                                    password=self.password,
+                                    host=self.host,
+                                    database=self.database)
+
+            cursor = conn.cursor()
+            query = """SELECT AVG(salary_from)
+                              FROM vacancies
+                              WHERE salary_from > 0"""
+            cursor.execute(query)
+            print(cursor.fetchall())
+        except (Exception, Error) as error:
+            print("Ошибка при работе с PostgreSQL", error)
+        finally:
+            if conn:
+                cursor.close()
+                conn.close()
+
+    def get_vacancies_with_higher_salary(self):
+        """Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям"""
+        try:
+            conn = psycopg2.connect(user=self.user,
+                                    password=self.password,
+                                    host=self.host,
+                                    database=self.database)
+
+            cursor = conn.cursor()
+            query = """SELECT title
+                       FROM vacancies
+                       WHERE salary_from > (SELECT AVG(salary_from) FROM vacancies WHERE salary_from > 0)"""
+            cursor.execute(query)
+            print(cursor.fetchall())
+        except (Exception, Error) as error:
+            print("Ошибка при работе с PostgreSQL", error)
+        finally:
+            if conn:
+                cursor.close()
+                conn.close()
+
+    def get_vacancies_with_keyword(self, keyword):
+        """
+        получает список всех вакансий, в названии которых содержатся переданные в метод слова,
+        например python
+        """
+        try:
+            conn = psycopg2.connect(user=self.user,
+                                    password=self.password,
+                                    host=self.host,
+                                    database=self.database)
+
+            cursor = conn.cursor()
+            query = f"SELECT title FROM vacancies WHERE title LIKE '%{keyword}%'"
+            cursor.execute(query)
+            print(cursor.fetchall())
+        except (Exception, Error) as error:
+            print("Ошибка при работе с PostgreSQL", error)
+        finally:
+            if conn:
+                cursor.close()
+                conn.close()
